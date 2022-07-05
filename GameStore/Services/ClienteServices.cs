7aa631075c1dc14b843754a1ebb6c36e3d9c2cf2
Pablo Cladera <83961694+Druklo@users.noreply.github.com>
2022-07-05 12:30:40 -0300

    Add files via upload

---
+++ GameStore/Services/ClienteServices.cs
@@ -0,0 +1,61 @@
+﻿using GameStore.Datos;
+using GameStore.Models;
+using GameStore.Models.DataTransfers;
+using GameStore.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameStore.Services
+{
+    public class ClienteServices : IClienteServices
+    {
+        private readonly GameStoreDB2Context _context;
+
+        public ClienteServices(GameStoreDB2Context context)
+        {
+            _context = context;
+        }
+
+        //public IEnumerable<Cliente> GetAll()
+        //{
+        //    return _context.Cliente.(c => c.Id).ToList();
+        //    //return _context.Cliente.Include(c => c.Id).ToList();
+        //}
+
+        public IEnumerable<Cliente> GetAll()
+        {
+            return _context.Cliente.Include(c => c.Id).ToList();
+        }
+
+        public Cliente GetOne(decimal clienteId)
+        {
+            
+            return _context.Cliente.SingleOrDefault(x => x.Id == clienteId);
+        }
+
+        public void DeleteCliente(Cliente cliente)
+        {
+            _context.Cliente.Remove(cliente);
+            _context.SaveChanges();
+        }
+
+        public Cliente UpdateCliente(ClienteCreateOrUpdateDate data)
+        {
+            var cliente = GetOne(data.Id);
+
+            if (cliente != null)
+            {
+                cliente.Id = data.Id;
+                cliente.IdPersona = data.IdPersona;
+
+                _context.SaveChanges();
+            }
+
+            return cliente;
+        }
+
+        public Cliente CreateCliente(ClienteCreateOrUpdateDate data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
