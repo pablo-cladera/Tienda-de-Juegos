c5725c965508d96058a73004f1c199d44fa8b313
Pablo Cladera <83961694+Druklo@users.noreply.github.com>
2022-07-11 18:14:27 -0300

    Add files via upload

---
+++ Web API Server/GameStore/Controllers/ClientesController.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using GameStore.Models;
+using GameStore.Models.DataTransfers;
+using GameStore.Services;
+using GameStore.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameStore.Controllers
+{
+    [ApiController]
+    [Route("Clientes")]
+    public class ClientesController : Controller
+    {
+        private readonly ILogger<ClientesController> _logger;
+
+        private IMapper _mapper;
+
+        private IClienteServices _clienteService;
+
+        public ClientesController(ILogger<ClientesController> logger,
+                                IMapper mapper,
+                                IClienteServices clienteService)
+        {
+            _logger = logger;
+            _mapper = mapper;
+            _clienteService = clienteService;
+
+        }
+
+
+        //private readonly GameStoreDBContext _context;
+
+        //public ClientesController(GameStoreDBContext context)
+        //{
+        //    _context = context;
+        //}
+
+        //[HttpGet]
+        //public async Task<IActionResult> Index()
+        //{
+        //    return Ok(_clienteService.Cliente.ToList());
+        //}
+
+        [Route("all")]
+        [HttpGet]
+        public IEnumerable<ClienteViewModel> GetALL()
+        {
+            var cliente = _clienteService.GetAll();
+            var clienteDTO = _mapper.Map<IEnumerable<ClienteViewModel>>(cliente);
+            return clienteDTO;
+        }
+    }
+}
---
+++ Web API Server/GameStore/Controllers/JuegosController.cs
@@ -0,0 +1,161 @@
+using AutoMapper;
+using GameStore.Models.DataTransfers;
+using GameStore.Services;
+using GameStore.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameStore.Controllers
+{
+    [ApiController]
+    [Route("Juegos")]
+    public class JuegosController : ControllerBase
+    {
+
+        private readonly ILogger<JuegosController> _logger;
+
+        private IMapper _mapper;
+
+        private IJuegoServices _juegoService;
+
+        public JuegosController(ILogger<JuegosController> logger,
+                                IMapper mapper,
+                                IJuegoServices juegoService)
+        {
+            _logger = logger;
+            _mapper = mapper;
+            _juegoService = juegoService;
+
+        }
+
+        [Route("all")]
+        [HttpGet]
+        public IEnumerable<JuegoViewModel> GetALL()
+        {
+            var juego = _juegoService.GetAll();
+            var juegoDTO = _mapper.Map<IEnumerable<JuegoViewModel>>(juego);
+            return juegoDTO;
+        }
+
+        [HttpGet("{Id}")]
+        public ActionResult<JuegoLiteDTO> GetOne(decimal Id)
+        {
+            var juego = _juegoService.GetOne(Id);
+            if (juego == null)
+            {
+                return NotFound();
+            }
+
+            var juegoDTO = _mapper.Map<JuegoLiteDTO>(juego);
+            return Ok(juegoDTO);
+        }
+
+        [Route ("byName")]
+        [HttpGet]
+
+        public ActionResult<JuegoViewModel>GetByName(string nombreJuego)
+        {
+            var juego = _juegoService.GetByName(nombreJuego);
+
+            if(juego == null)
+            {
+                return NotFound();
+            }
+
+            var juegoDTO = _mapper.Map<IEnumerable<JuegoViewModel>>(juego);
+
+            return Ok(juegoDTO);
+        }
+
+
+        [HttpDelete("{Id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public IActionResult Delete(decimal Id)
+        {
+            var juego = _juegoService.GetOne(Id);
+
+            if( juego == null)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                _juegoService.DeleteJuego(juego);
+                return Ok();
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                                    "Error deleting Data");
+            }
+        }
+
+        [Route("update")]
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public ActionResult<JuegoLiteDTO>UpdateJuego(JuegoCreateOrUpdateDate juegoData)
+        {
+            if (juegoData == null)
+            {
+                return StatusCode(StatusCodes.Status406NotAcceptable,
+                                  "JuegoData can't be null");
+            }
+
+            try
+            {
+                var juego = _juegoService.UpdateJuego(juegoData);
+                if (juego == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return Ok(_mapper.Map<JuegoViewModel>(juego));
+                }
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                                 "Error deleting data");
+            }
+        }
+
+        [Route("create")]
+        [HttpPut]
+
+        public ActionResult<JuegoLiteDTO>CreateJuego(JuegoCreateOrUpdateDate juegoData)
+        {
+            if(juegoData == null)
+            {
+                return StatusCode(StatusCodes.Status406NotAcceptable,
+                                  "Juego data can't be null");
+            }
+
+            try
+            {
+                var juego = _juegoService.CreateJuego(juegoData);
+                if (juego == null)
+                {
+                    return StatusCode(StatusCodes.Status400BadRequest, "Unable to create game");
+                }
+                else
+                {
+                    return Ok(_mapper.Map<JuegoViewModel>(juego));
+                }
+            }
+            catch (Exception Ex)
+            {
+                _logger.LogError(Ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                                    "Error Deleting Data");
+            }
+        }
+        
+    }
+}
